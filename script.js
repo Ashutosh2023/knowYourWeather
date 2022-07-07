96e4d681f1937165056ae0671eb10a4781858ee9
Ashutosh2023 <76429779+Ashutosh2023@users.noreply.github.com>
2022-07-07 18:41:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let weather = {
+    apiKey: "6f5d203c6cf763fd9dde29642da89d44",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+  
+      //Remove the loading css for the page when the when gets loaded for the default city Below
+      document.querySelector(".weather").classList.remove("loading");
+      
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  /*This part makes the search button work.
+  When you click the search button you call the search function below display weather*/
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  /*This part is to search the city using the enter key*/
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+//   weather.fetchWeather("Denver");
+  
